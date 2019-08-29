45deffc8d22b739bd6c1ac61b1726905aaa26380
lgxkevin <lgx9587@gmail.com>
2019-08-29 22:24:35 +1200

    update

--- jupiterContext/jupiterContext.cs
+++ jupiterContext/jupiterContext.cs
@@ -37,7 +37,7 @@ namespace jupiterCore.jupiterContext
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySQL("server=35.197.166.191;port=3306;user=dbuser;password=ToMPyaJzCW88JPRqBkxqZpiiEElX7Tv1;database=jupiter");
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=dbuser;password=ToMPyaJzCW88JPRqBkxqZpiiEElX7Tv1;database=jupiter");
             }
         }
 
